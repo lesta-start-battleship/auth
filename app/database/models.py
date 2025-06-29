5e2703751bc29e1808c9fa205b6622c911980328
Darkvran <darkvran2019@gmail.com>
2025-06-29 20:41:46 +0300

    curr col

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, Enum as SQLEnum, ForeignKey
 from datetime import datetime
 from enum import Enum
@@ -11,9 +11,18 @@ class Role(Enum):
 
 
 class Base(DeclarativeBase):
-    __abstract__ = True 
+    __abstract__ = True
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+
+class UserCurrency(Base):
+    __tablename__ = "balance"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    silver: Mapped[int] = mapped_column(default=0)
+    gold: Mapped[int] = mapped_column(default=0)
+    created_at: Mapped[str] = mapped_column(default=datetime.now)
 
 
 class UserBase(Base):
@@ -25,12 +34,6 @@ class UserBase(Base):
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     h_password: Mapped[str] = mapped_column(String(50), nullable=True)
     is_active: Mapped[bool] = mapped_column(nullable=False)
-    role: Mapped[Role] = mapped_column(SQLEnum(Role, name="role_enum"))  
+    role: Mapped[Role] = mapped_column(SQLEnum(Role, name="role_enum"))
+    currencies: Mapped[UserCurrency] = relationship("Currency", back_populates="user", cascade="all, delete-orphan")
     created_at: Mapped[str] = mapped_column(default=datetime.now)
-
-class Balance(Base):
-    __tablename__ = "balance"
-
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
-    silver: Mapped[int] = mapped_column(default=0)
-    gold: Mapped[int] = mapped_column(default=0)
