d04d73bda7be3855239124078907ba97ee38b769
Sladkiy-bubalex <shadow.ru_93@mail.ru>
2025-06-29 21:23:23 +0300

    Conflict resolution

--- app/config.py
+++ app/config.py
@@ -6,3 +6,31 @@ dotenv.load_dotenv(env_file, override=True)
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler("app.log", maxBytes=20000, backupCount=2)
+logger.addHandler(handler)
+
+jwt = JWTManager()
+
+load_dotenv()
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+VERSION = os.getenv("VERSION")
+
+JWT_ACCESS_BLOCKLIST = os.getenv("JWT_ACCESS_BLOCKLIST", timedelta(hours=1))
+JWT_ACCESS_TOKEN_EXPIRES = os.getenv("JWT_ACCESS_TOKEN_EXPIRES", timedelta(days=1))
+JWT_REFRESH_TOKEN_EXPIRES = os.getenv("JWT_REFRESH_TOKEN_EXPIRES", timedelta(days=7))
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
