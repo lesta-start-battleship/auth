4592e331ca387070aedfeab626ad499a84d257c1
AmalSultanov <pogboom1014@gmail.com>
2025-06-30 13:12:39 +0500

    Rename session to async_session

--- app/database/database.py
+++ app/database/database.py
@@ -4,4 +4,4 @@ from app.config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
 DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}"
 
 engine = create_async_engine(DSN, echo=True)
-session = async_sessionmaker(bind=engine, expire_on_commit=False)
+async_session = async_sessionmaker(bind=engine, expire_on_commit=False)
