00c2ec411be213667f304283a4135430af57f25f
AmalSultanov <pogboom1014@gmail.com>
2025-07-06 13:17:39 +0500

    fix(app): Use env variables to construct postgres DSN

--- app/database/database.py
+++ app/database/database.py
@@ -1,11 +1,13 @@
-from config import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
+from config import (
+    POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_HOST, POSTGRES_PORT
+)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 DSN = (
     f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@"
-    f"localhost:5432/{POSTGRES_DB}"
+    f"{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 )
 
 engine = create_engine(DSN)
