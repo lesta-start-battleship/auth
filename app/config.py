d715c165aaa4111c3129137efc8558c76b887f0e
AmalSultanov <pogboom1014@gmail.com>
2025-07-08 00:02:53 +0500

    fix(app): Cast string numbers from env vars into integers

--- app/config.py
+++ app/config.py
@@ -26,14 +26,14 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 
 VERSION = os.getenv("VERSION")
 
-JWT_ACCESS_BLOCKLIST = os.getenv("JWT_ACCESS_BLOCKLIST", timedelta(hours=1))
-JWT_ACCESS_TOKEN_EXPIRES = os.getenv(
-    "JWT_ACCESS_TOKEN_EXPIRES",
-    timedelta(days=1)
+JWT_ACCESS_BLOCKLIST = timedelta(
+    hours=int(os.getenv("JWT_ACCESS_BLOCKLIST", 1))
 )
-JWT_REFRESH_TOKEN_EXPIRES = os.getenv(
-    "JWT_REFRESH_TOKEN_EXPIRES",
-    timedelta(days=7)
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+    days=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 1))
+)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+    days=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES", 7))
 )
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
