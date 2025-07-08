faa07421cc71dbc4ab74b955eb3f90c99e9beb86
Sladkiy-bubalex <shadow.ru_93@mail.ru>
2025-07-08 20:52:29 +0300

    - fix(app): Fixing the error in the kafka pod in auth-deployment.yaml file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/) 
 
 ---
 
+## [1.1.4] - (2025-07-08)
+
+### Fix
+
+- fix(app): Fixing the error in the kafka pod in auth-deployment.yaml file
+
 ## [1.1.3] - (2025-07-08)
 
 ### Fix
--- app/signals.py
+++ app/signals.py
@@ -29,7 +29,7 @@ def user_registered_handler(sender, **kwargs):
         "Чтобы активировать вашу учетную запись, "
         "пожалуйста, подтвердите свой адрес электронной почты, "
         "перейдя по следующей ссылке:\n\n"
-        f"https://battleship-lesta-start-auth.ru/api/v1/auth/confirm_email/{confirm_code}\n\n"
+        f"https://battleship-lesta-start-auth.ru/auth/confirm_email/{confirm_code}\n\n"
         "С уважением,\n"
         "Команда игры Морской бой"
     )
